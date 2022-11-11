3a27e459d86074059e4140d03ff9d51f4ad09401
greatghoul <greatghoul@gmail.com>
2022-11-11 19:29:21 +0800

    implement closing tab

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Build the package
 Build the package on windows
 
     npm run buildw
+
+## Usage
+
+Install the extension and navigate to [chrome://extensions/shortcuts](chrome://extensions/shortcuts) to setup ctrl+w or cmd+w to "Close Active Editor"
+
+![](https://i.imgur.com/9ll3ByG.png)
--- app/scripts/event.js
+++ app/scripts/event.js
@@ -1,7 +1,35 @@
+function attemptToCloseActiveEditor () {
+  try {
+    const isWorkbench = document.querySelector("#vscode-workbench-web-configuration");
+    const activeTab = document.querySelector(".tab.active");
+    if (isWorkbench && activeTab) {
+      activeTab.querySelector(".action-item").click();
+      return true;
+    }
+  } catch (e) {}
+
+  return false;
+}
+
 function openExtensionShortcutSettings () {
   chrome.tabs.create({
     url: "chrome://extensions/shortcuts"
   });
 }
 
+function handleCommand (command, tab) {
+  if (command == "close-editor") {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: attemptToCloseActiveEditor,
+    }, results => {
+      // force close the tab on failed
+      if (!results || !results[0].result) {
+        chrome.tabs.remove(tab.id);
+      }
+    });
+  }
+}
+
 chrome.action.onClicked.addListener(openExtensionShortcutSettings);
+chrome.commands.onCommand.addListener(handleCommand);
